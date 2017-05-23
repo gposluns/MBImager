bb004341fccdd8b7b3d90f80bbccd20d049787e7
MBImager <MBImager>
2017-05-23 16:50:34 -0400

    addign opalkelly peripherals, still need bb linking

---
+++ peripherals/MyProcessorIPLib/drivers/oktriggerins_v1_00_a/data/oktriggerins_v2_1_0.tcl
@@ -0,0 +1,11 @@
+##############################################################################
+## Filename:          C:\Users\MBImager\Documents\MBImager_ams\repo\MBImager\peripherals\MyProcessorIPLib/drivers/oktriggerins_v1_00_a/data/oktriggerins_v2_1_0.tcl
+## Description:       Microprocess Driver Command (tcl)
+## Date:              Tue May 23 11:06:23 2017 (by Create and Import Peripheral Wizard)
+##############################################################################
+
+#uses "xillib.tcl"
+
+proc generate {drv_handle} {
+  xdefine_include_file $drv_handle "xparameters.h" "oktriggerins" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" 
+}
