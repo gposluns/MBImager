bb002b0ec4317ffe2d4e69ba9843d4d89f9ba3da
MBImager <MBImager>
2017-06-02 10:09:41 -0400

    removed non-functional v1 peripherals

---
+++ peripherals/myprocessoriplib/drivers/oktriggerins_v3_00_a/src/oktriggerins.c
@@ -0,0 +1,14 @@
+/*****************************************************************************
+* Filename:          C:\Users\MBImager\Documents\MBImager_ams\repo\MBImager\peripherals\MyProcessorIPLib/drivers/oktriggerins_v3_00_a/src/oktriggerins.c
+* Version:           3.00.a
+* Description:       oktriggerins Driver Source File
+* Date:              Fri Jun 02 10:01:18 2017 (by Create and Import Peripheral Wizard)
+*****************************************************************************/
+
+
+/***************************** Include Files *******************************/
+
+#include "oktriggerins.h"
+
+/************************** Function Definitions ***************************/
+
