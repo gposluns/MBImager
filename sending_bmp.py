37d1acf835d15264e64c78afa38401aa0b95bc4e
jackieke724 <jackieke724@hotmail.com>
2017-06-05 16:57:59 -0400

    python code for reading bmp files

--- sending_bmp.py
+++ sending_bmp.py
@@ -1,11 +1,33 @@
-
+import struct
 
 file = open("half_black.bmp","rb")
-image = file.read()
-for i in image[10:14]:
-    print i
-    print ord(i)
-
-print bin(ord(image[62]))
-print ord(image[63])
+image = bytearray(file.read())
 file.close()
+
+#print struct.unpack_from('<L',image, 10)
+print struct.unpack_from('<L', image, 10)[0]
+print "image property: ", len(image), type(image)
+for i in range(62,66):
+    print i, bin(image[i]), image[i], struct.unpack_from('<H', image, i)
+
+
+#get start index of the pixel array
+pix_start_pos = struct.unpack_from('<L', image, 10)[0]
+
+#get the width
+pat_width = struct.unpack_from('<L', image, 18)[0]
+
+#get the height
+pat_height = struct.unpack_from('<L', image, 22)[0]
+
+print pix_start_pos, pat_width, pat_height
+counter = 0
+
+#generate the pattern
+pattern = bytearray();
+for i in range(pix_start_pos, pix_start_pos+pat_height*pat_width/4):
+    if (image[i] != 0):
+        pattern.append(image[i])
+
+        
+print len(pattern)
