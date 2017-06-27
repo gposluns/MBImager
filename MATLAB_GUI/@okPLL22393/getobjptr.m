d4bc6a9a27c92fc71ac8a2412833530608fa129b
MBImager <MBImager>
2017-06-27 11:15:21 -0400

    removed all nonessential things

--- MATLAB_GUI/@okPLL22393/getobjptr.m
+++
@@ -1,7 +0,0 @@
-function p = getobjptr(obj)
-
-%
-%  Copyright (c) 2005 Opal Kelly Incorporated
-%  $Rev: 236 $ $Date: 2005-12-10 14:32:28 -0800 (Sat, 10 Dec 2005) $
-
-p = obj.ptr;
--- MATLAB_GUI/Image_creator.m
+++
@@ -1,3 +0,0 @@
-image = zeros(1024,1280);
-% image(463:562,591:690) = ones(100,100);
-imwrite(image,'SquareZeros.png')
